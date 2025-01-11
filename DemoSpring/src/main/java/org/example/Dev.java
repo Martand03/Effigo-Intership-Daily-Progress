27e6bc75affafc626f2865b56be135fda0dff982
Martand Mahajan <martandmahajan03@gmail.com>
2025-01-11 09:55:07 +0530

    Date-11/1/25 commit #1

--- DemoSpring/src/main/java/org/example/Dev.java
+++ DemoSpring/src/main/java/org/example/Dev.java
@@ -3,12 +3,28 @@ package org.example;
 public class Dev {
 
     private int age;
+    private Laptop laptop;
 
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
 
     // constructor injection
     public Dev(int age){
         this.age = age;
     }
+
+//    public Dev(laptop laptop){
+//        this.laptop = laptop;
+//        System.out.println("Dev parameter constructor");
+//    }
+//    public Laptop getLaptop() {
+//        return laptop;
+//    }
+//
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
 
     // settter injection
     public int getAge() {
