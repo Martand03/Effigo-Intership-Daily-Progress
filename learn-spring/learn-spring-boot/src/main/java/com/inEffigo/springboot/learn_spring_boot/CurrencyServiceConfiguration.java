f41c5ada1b9aeaaf662ed0aa3f7751f52b71a2ef
Martand Mahajan <martandmahajan03@gmail.com>
2025-01-18 12:39:39 +0530

    completion of third & fourth module

---
+++ learn-spring/learn-spring-boot/src/main/java/com/inEffigo/springboot/learn_spring_boot/CurrencyServiceConfiguration.java
@@ -0,0 +1,40 @@
+package com.inEffigo.springboot.learn_spring_boot;
+
+//currency-service.url=
+//currency-service.username=
+//currency-service.key=
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "currency-service")
+@Component
+public class CurrencyServiceConfiguration {
+    private String url;
+    private String name;
+    private String key;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
