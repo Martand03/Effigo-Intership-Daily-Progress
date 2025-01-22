f144a0251577f2a044f9a8fff050ad0fd13086dc
Martand Mahajan <martandmahajan03@gmail.com>
2025-01-23 01:03:42 +0530

    spring-boot using rest

---
+++ learn-spring/restful-web-services/src/main/java/com/inEffigo/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.inEffigo.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// rest api
+@RestController
+public class HelloWorldController {
+
+//    @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
+    @GetMapping(path = "hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(String name){
+        return new HelloWorldBean("Hello World");
+    }
+
+}
