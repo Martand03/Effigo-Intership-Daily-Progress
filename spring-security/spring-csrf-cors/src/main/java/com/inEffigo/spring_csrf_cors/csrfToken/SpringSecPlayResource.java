819513df10fe30927c02eff94c54f5b3480aac59
Martand Mahajan <martandmahajan03@gmail.com>
2025-02-03 14:36:58 +0530

    csrf and cors learnings

---
+++ spring-security/spring-csrf-cors/src/main/java/com/inEffigo/spring_csrf_cors/csrfToken/SpringSecPlayResource.java
@@ -0,0 +1,14 @@
+package com.inEffigo.spring_csrf_cors.csrfToken;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSecPlayResource {
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
