95a58e942d224a2fd15834cf5cbb7352b0ed9292
Martand Mahajan <martandmahajan03@gmail.com>
2025-02-04 17:12:54 +0530

    file changes

--- crud-with-dto-mappers/src/main/java/com/inEffigo/crud_with_dto_mappers/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inEffigo.crud_with_dto_mappers.repository;
-
-import com.inEffigo.crud_with_dto_mappers.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
